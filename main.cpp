63b31d9f733f03c0007e0ca8383bba48f7a80926
Ambrose Hundal <amshunda@ucsc.edu>
2018-02-12 17:09:14 -0800

    update with base class pointers and functions modified

--- main.cpp
+++ main.cpp
@@ -1,47 +1,53 @@
-/*
+
 Ambrose Hundal
 Stephen Lee
 CIS 22C
 Winter 18
-
 Lab 2b- QueueADT
-
 */
 
 
 
 #include <iostream>
-#include "Queue.h"
-#include "List.h"
+#include <string>
 #include "Currency.h"
 #include "Dollar.h"
+#include "List.h"
+#include "Queue.h"
+
 
 using namespace std;
 
 
-void intEnqueue(); 
-void intDequeue();
-void intQueueClear();
-void stringEnqueue();
-void stringDequeue();
-void stringClear();
-void currencyEnqueue();
-void currencyDequeue();
-void currencyClear();
+void intEnqueue(Queue<int> *intQptr);
+void intDequeue(Queue<int> *intQptr);
+void intQueueClear(Queue<int> *intQptr);
+void stringEnqueue(Queue<string> *stringQptr);
+void stringDequeue(Queue<string> *stringQptr);
+void stringClear(Queue<string> *stringQptr);
+void currencyEnqueue(Queue<Currency> *currencyQptr);
+void currencyDequeue(Queue<Currency> *currencyQptr);
+void currencyClear(Queue<Currency> *currencyQptr);
 
 
 
 
 
-int main(){
-Queue<int>intQueue;
-Queue<string>stringQueue;
-Queue<Currency>CurrencyQueue;
+int main() {
+	Queue<int>intQueue;
+	Queue<string>stringQueue;
+	Queue<Currency>CurrencyQueue;
 
+	
+
+
+
+	int choice = 0;
 	cout << "Welcome to Queue ADT implementation. ";
 	bool loopMenu = true;
 	while (loopMenu)
 	{
+
 		cout << "Please choose an operation from the options listed below:" << endl
 			<< "1. Enqueue an integer to the queue" << endl
 			<< "2. Dequeue an integer from the queue" << endl
@@ -53,67 +59,70 @@ Queue<Currency>CurrencyQueue;
 			<< "8. Dequeue an entry from currency queue" << endl
 			<< "9. Clear the Currency queue" << endl
 			<< "10. Exit Program" << endl;
-		
+
 		cin >> choice;
-		if (cin.fail ())
+		if (cin.fail())
 		{
-			cin.clear (); 
+			cin.clear();
 			choice = 0;
 		}
 		cout << endl;
-			
-			if (choice == 1) intEnqueue();
-			else if (choice == 2) intDequeue();
-			else if (choice == 3) intQueueClear();
-			else if (choice == 4) stringEnqueue();
-			else if (choice == 5) stringDequeue();
-			else if (choice == 6) stringClear();
-			else if (choice == 7) currencyEnqueue();
-			else if (choice == 8) currencyDequeue();
-			else if (choice == 9) currencyClear();
-			else if (choice == 10) loopActive = false;
-		}
+
+		if (choice == 1) intEnqueue(&intQueue);
+		else if (choice == 2) intDequeue(&intQueue);
+		else if (choice == 3) intQueueClear(&intQueue);
+		else if (choice == 4) stringEnqueue(&stringQueue);
+		else if (choice == 5) stringDequeue(&stringQueue);
+		else if (choice == 6) stringClear(&stringQueue);
+		else if (choice == 7) currencyEnqueue(&CurrencyQueue);
+		else if (choice == 8) currencyDequeue(&CurrencyQueue);
+		else if (choice == 9) currencyClear(&CurrencyQueue);
+		else loopMenu = false;
 	}
+	
+
+	return 0;
 }
 
-  
-      
+
+
+
 
 //intEnqueue
 //enters an integer into the queue
-void intEnqueue ()
+void intEnqueue(Queue<int> *intQptr)
 {
- int x;
-  cout << "Enter a number to add to the queue: ";
-  cin >> x;
-	intQueue->enqueue(x);
-	cout << "Queue items:" << endl << intQueue << endl;
+	int x;
+	cout << "Enter a number to add to the queue: ";
+	cin >> x;
+	intQptr->enqueue(x);
+	cout << "Queue items:" << endl << intQptr << endl;
 }
 
 
 //intDequeue         
 //remove an entry from the queue
-void intDequeue ()
+void intDequeue(Queue<int> *intQptr)
 {
-	if (intQueue->empty ())
+	if (intQptr->empty())
 	{
-		
+
 		cout << "Queue is empty!" << endl;
 	}
 	else
 	{
-		intQueue->dequeue();
-		cout << "Integer Queue:" << endl << intQueue << endl << endl;
+		intQptr->dequeue();
+		cout << "Integer Queue:" << endl << intQptr << endl << endl;
 	}
 }
 
 
 //ClearQueue           
 //clears int queue
-void ClearintQueue ()
+void ClearintQueue(Queue<int> *intQptr)
 {
-	intQueue->clear ();
-	cout << "Queue contains:" << endl << intQueue << endl << endl;
+	intQptr->clear();
+	cout << "Queue contains:" << endl << intQptr << endl << endl;
 }
 
 
@@ -122,7 +131,7 @@ void ClearintQueue ()
 void stringEnqueue()
 {
 	string buffer;
-	ifstream read_input_file ("demo.txt");
+	ifstream read_input_file("demo.txt");
 
 	cout << "Adding \"demo.txt\" contents to queue:" << endl;
 	while (true)
@@ -131,32 +140,32 @@ void stringEnqueue()
 		if (!read_input_file) break;
 		stringQueue->enqueue(buffer);
 	}
-	read_input_file.close ();
-	cout << "Queue string items:" << endl << stringQueue << endl << endl;
+	read_input_file.close();
+	cout << "Queue string items:" << endl << stringQptr << endl << endl;
 }
 
 
 //stringDequeue              
 //removes a string from the queue
 
-void stringDequeue()
+void stringDequeue((Queue<string> *stringQptr)
 {
-	if (stringDequeue->empty ())	{
-  cout << "Queue is empty!" << endl;
+	if (*stringQptr->empty()) {
+		cout << "Queue is empty!" << endl;
 	}
 	else
 	{
-		stringQueue->dequeue ();
-		cout << "Queue contains:" << endl << stringQueue << endl << endl;
+		stringQptr->dequeue();
+		cout << "Queue contains:" << endl << stringQptr << endl << endl;
 	}
 }
 
 //stringQueueclear              
-// Clears the string queue
-void stringQueueClear ()
+//Clears the string queue
+void stringQueueClear()
 {
-	stringQueue->clear ();
-	cout << "string stack items:" << endl << stackString << endl << endl;
+	stringQueue->clear();
+	cout << "string stack items:" << endl << stringQptr << endl << endl;
 }
 
 
@@ -164,18 +173,18 @@ void stringQueueClear ()
 //enters a currency to the queue
 void currencyEnqueue()
 {
-	
- currencyQueue->enqueue(Dollar(3, 35));
-	
-	cout << "Queue currency contains:" << endl << CurrencyQueue << endl << endl;
+
+	currencyQueue->enqueue(Dollar(3, 35));
+
+	cout << "Queue currency contains:" << endl << CurrencyQptr << endl << endl;
 }
 
 
 //currencyDequeue      
 //removes a currency object from the queue
-void currencyDequeue ()
+void currencyDequeue()
 {
-	if (currencyQueue->empty ())
+	if (currencyQueue->empty())
 	{
 		cout << "Queue is empty!" << endl << endl;
 	}
@@ -191,10 +200,10 @@ void currencyDequeue ()
 
 //currencyClear             
 //clears the currency queue
-currencyClear()
+void currencyClear()
 {
-	currencyQueue->clear ();
-	cout << "Currency iters:" << endl << currencyQueue << endl << endl;
+	currencyQueue->clear();
+	cout << "Currency items:" << endl << currencyQueue << endl << endl;
 }
 
 
@@ -204,12 +213,12 @@ currencyClear()
 template <class T>
 std::ostream& operator<< (std::ostream &foo, List<T> *ListPtr)
 {
-	
+
 	int itemCount = 0;
-	if (ListPtr->empty ()) cout << "List is empty" << endl;
+	if (ListPtr->empty()) cout << "List is empty" << endl;
 	else
 	{
-		Node<T> *currPtr = ListPtr->getTail ();
+		Node<T> *currPtr = ListPtr->getTail();
 		while (currPtr != nullptr)
 		{
 			itemCount++;
@@ -222,13 +231,12 @@ std::ostream& operator<< (std::ostream &foo, List<T> *ListPtr)
 template <class T>
 std::ostream& operator<< (std::ostream &foo, Queue<T> *ListPtr)
 {
-	// Since operator<< is a friend of the List class, we can access
-	// it's members directly.
+	
 	int itemCount = 0;
-	if (ListPtr->empty ()) cout << "List is empty" << endl;
+	if (ListPtr->empty()) cout << "List is empty" << endl;
 	else
 	{
-		Node<T> *currPtr = ListPtr->getTail ();
+		Node<T> *currPtr = ListPtr->getTail();
 		while (currPtr != nullptr)
 		{
 			itemCount++;
@@ -244,8 +252,3 @@ std::ostream& operator<< (std::ostream &foo, Queue<T> *ListPtr)
 
 
 
-
-
-
-
-}
